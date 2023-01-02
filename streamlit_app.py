3b5608cc06b24c1706d533a37ccf991eba4480c6
harpal-s-kalsi <68592181+harpal-s-kalsi@users.noreply.github.com>
2023-01-02 16:09:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,9 @@ streamlit.dataframe(fruits_to_show)
 
 def get_fruityvice_data(this_fruit_choice):
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    # write your own comment -what does the next line do? 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
+   # write your own comment -what does the next line do? 
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
   
 
 streamlit.header("Fruityvice Fruit Advice!")
